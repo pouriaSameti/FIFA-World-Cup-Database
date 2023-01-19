1f3d178e233d00da2d6679c7a765019f3478aeb2
pouria <pouria.sameti80@gmail.com>
2023-01-19 05:36:08 +0330

    query(3) was chnaged

--- phase 2/query/3.sql
+++ phase 2/query/3.sql
@@ -1,6 +1,8 @@
-SELECT *
+SELECT h.name, g.minute
 FROM player AS p
 JOIN goal AS g
 ON p.id = g.player_id
-WHERE g.minute > 80
+JOIN human AS h
+ON h.id = p.id
+WHERE g.minute >= 80
 GROUP BY(p.id)
